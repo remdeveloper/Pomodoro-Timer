880bceffe09518afceabb2122a4f1ee582315e7a
unknown <remusblee@gmail.com>
2021-05-06 14:45:39 -0400

    edited for deployment

---
+++ build/precache-manifest.ab68ce4fde0878b870e521d2ad2f6a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b0fbe4b8febf23668c3b9668f376ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf9b1da177a7e85790f",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf9b1da177a7e85790f",
+    "url": "/static/js/main.09a8797d.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
